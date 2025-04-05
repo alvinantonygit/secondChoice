1d6d7fa03b641b75b09988c1811c64b13acdea44
Alvin Antony <alvinantony0467@gmail.com>
2025-04-05 20:26:12 +0530

    make models for Team members

--- secondChoice/pages/models.py
+++ secondChoice/pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Teams(models.Model):
+    first_name=models.CharField(max_length=255)
+    lase_name=models.CharField(max_length=255)
+    designation=models.CharField(max_length=100)
+    photo=models.ImageField(upload_to='photos/%y/%m/%d')
+    facebook_link=models.URLField(max_length=100)
+    twitterLink=models.URLField(max_length=100)
+    google_page=models.URLField(max_length=100)
+    created_date=models.DateTimeField(auto_now_add=True)
+
+
