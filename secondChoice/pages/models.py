b4ecf7ad6f1ea4f53ee4ec35fb1ca54c8a8cf874
Alvin Antony <alvinantony0467@gmail.com>
2025-04-05 22:14:19 +0530

    admin pannel changes and migration /completed team model

--- secondChoice/pages/models.py
+++ secondChoice/pages/models.py
@@ -2,14 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class Teams(models.Model):
+class Team(models.Model):
     first_name=models.CharField(max_length=255)
-    lase_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
     designation=models.CharField(max_length=100)
     photo=models.ImageField(upload_to='photos/%y/%m/%d')
     facebook_link=models.URLField(max_length=100)
     twitterLink=models.URLField(max_length=100)
     google_page=models.URLField(max_length=100)
     created_date=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.first_name
 
 
