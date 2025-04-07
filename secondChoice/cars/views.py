318b32fe2c90a663f9bd5a60c347d4290fc2e517
Alvin Antony <alvinantony0467@gmail.com>
2025-04-07 12:11:33 +0530

    few errors in pagination and paginator

--- secondChoice/cars/views.py
+++ secondChoice/cars/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Car
+from django.core.paginator import EmptyPage,PageNotAnInteger
 
 # Create your views here.
 def cars(request):
-    return render(request,'cars/cars.html')
+    cars=Car.objects.order_by('-created_date')
+
+    
+    paginator=paginator(cars,5)
+    page=request.GET.get('page')
+
+    try:
+
+       paged_cars=paginator.get.page(page)
+
+    except PageNotAnInteger:
+        paged_cars=paginator.page(1)
+    except EmptyPage:
+        paged_cars=paginator.page(paginator.num_pages)
+    data={
+        'cars':paged_cars,
+    }
+
+    return render(request,'cars/cars.html',data)
+
 
 def car_detail(request,id):
 
@@ -12,3 +32,6 @@ def car_detail(request,id):
         'single_car':single_car,
     }
     return render(request,'cars/car_detail.html',data)
+
+def search(request):
+    return render(request,'cars/search.h')
