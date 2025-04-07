0a2aaaa4711817e0df0d3a4a93abc5cb818f30fd
Alvin Antony <alvinantony0467@gmail.com>
2025-04-07 22:14:15 +0530

    pagination applied

--- secondChoice/cars/views.py
+++ secondChoice/cars/views.py
@@ -1,37 +1,28 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Car
-from django.core.paginator import EmptyPage,PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def cars(request):
-    cars=Car.objects.order_by('-created_date')
+    cars = Car.objects.order_by('-created_date')
+    paginator = Paginator(cars, 5)  # Fixed: capital 'P' for Paginator
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)  # Fixed: correct method
 
-    
-    paginator=paginator(cars,5)
-    page=request.GET.get('page')
-
-    try:
-
-       paged_cars=paginator.get.page(page)
-
-    except PageNotAnInteger:
-        paged_cars=paginator.page(1)
-    except EmptyPage:
-        paged_cars=paginator.page(paginator.num_pages)
-    data={
-        'cars':paged_cars,
+    data = {
+        'cars': paged_cars,
     }
 
-    return render(request,'cars/cars.html',data)
+    return render(request, 'cars/cars.html', data)
 
 
-def car_detail(request,id):
-
-    single_car=get_object_or_404(Car,pk=id)
-    data={
-        'single_car':single_car,
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+    data = {
+        'single_car': single_car,
     }
-    return render(request,'cars/car_detail.html',data)
+    return render(request, 'cars/car_detail.html', data)
+
 
 def search(request):
-    return render(request,'cars/search.h')
+    return render(request, 'cars/search.html')  # Fixed: file extension
