5ea8f0585bf72bc2ddb0b6a62e7a55aec08ef8b6
Alvin Antony <alvinantony0467@gmail.com>
2025-04-14 15:54:01 +0530

    modified and create admin file ,completed search func and admin customization

--- secondChoice/pages/views.py
+++ secondChoice/pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Team
 from cars.models import Car
+from django.db.models.functions import Lower
+
 
 # Create your views here.
 
@@ -8,10 +10,25 @@ def home(request):
     teams=Team.objects.all()
     featured_cars=Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars=Car.objects.order_by('-created_date')
+    # search_fields=Car.objects.values('model','city','year','body_style','price')
+    search_model=Car.objects.values_list('model',flat=True).distinct()
+    search_city=Car.objects.values_list('city',flat='True').distinct()
+    search_year=Car.objects.values_list('year',flat=True).distinct()
+    search_body_style=Car.objects.annotate(body_style_lower=Lower('body_style')).values_list('body_style_lower',flat=True).distinct()
+
+
+
     data={
         'teams':teams,
         'featured_cars':featured_cars,
         'all_cars':all_cars,
+        'search_model':search_model,
+        'search_city':search_city,
+        'search_year':search_year,
+        'search_body_style':search_body_style,
+        
+
+
 
     }
     return render(request,'pages/home.html',data)
