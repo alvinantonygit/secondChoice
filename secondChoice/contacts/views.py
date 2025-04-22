03b9be45323ec4ff68fec3f000081e31fdab759d
Alvin Antony <alvinantony0467@gmail.com>
2025-04-22 07:34:45 +0530

    email to admin setup

--- secondChoice/contacts/views.py
+++ secondChoice/contacts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect,render
 from .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
 
 
 
@@ -20,10 +22,28 @@ def inquiry(request):
         car_id=request.POST.get('car_id')
         phone=request.POST.get('phone')
 
+        if request.user.is_authenticated:
+            user_id=request.user.id
+            has_contacted=Contact.objects.all().filter(car_id=car_id,user_id=user_id)
 
+            if has_contacted:
+                messages.error(request,'You already sent a message .Our team will contact  you soon ')
+                return redirect('/cars/'+car_id)
+           
         contact = Contact(car_id=car_id, car_title=car_title, user_id=user_id,
         first_name=first_name, last_name=last_name, customer_need=customer_need, city=city,
         state=state, email=email, phone=phone, message=message)
+
+        admin_info=User.objects.get(is_superuser=True)
+        admin_email=admin_info.email
+
+        send_mail(
+            "New  Car Inquiry ",
+            "You have been new inquiry for" +car_title+".please login to get more info",
+            "alvinantony2112@gmail.com",
+            [admin_email],
+            fail_silently=False,
+        )
 
         contact.save()
         messages.success(request,'Your message submitted ')
--- secondChoice/secondChoice/settings.py
+++ secondChoice/secondChoice/settings.py
@@ -174,3 +174,14 @@ MESSAGE_TAGS = {
 }
 
 SITE_ID=1
+
+
+
+# Email send setting 
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'alvinantony2112@gmail.com'
+EMAIL_HOST_PASSWORD = 'jskk hwkk tauz rwaw'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
